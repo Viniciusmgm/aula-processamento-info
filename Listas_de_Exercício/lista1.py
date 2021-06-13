4205446656fb3794573f0d1dcb72b44b10185628
Viniciusmgm <vinicius.mirandagm@gmail.com>
2021-06-13 01:02:16 -0300

    finalização exerc1

--- Listas_de_Exercício/lista1.py
+++ Listas_de_Exercício/lista1.py
@@ -5,30 +5,50 @@
 """
 Entrada: posições de x e y da flecha
 Saída: posição da flecha no alvo 
-
+inicio (dentro do método main())
+  recebe posição x
+  recebe posição y
+  calcula a distãncia entre (x,y) e a origem
+  criação do método posição
+  chamada do método posição
+    se a distância for menor que ou igual a 1
+      retorna que está na região A
+    se a distância for menor que ou igual a 2
+      retorna que está na região B
+    se a distância for menor que ou igual a 3
+      retorna que está na região C
+    se a distância for menor que ou igual a 4
+      retorna que está na região D
+    se a distância for menor que ou igual a 5
+      retorna que está na região E
+    senão
+      retorna que está fora do alvo
+  fim do método posição
+  imprime região na tela
+fim
 """
 
 #b)Código em Python:
 """
-Está errado!!!
-def posicao(x,y):
-  if((x <= 1 and x >= -1) and (y <= 1 and y >= -1)):
+def posicao(dist):
+  if(dist <= 1):
     return "A flecha atingiu a região A"
-  elif((x <= 2 and x >= -2) and (y <= 2 and y >= -2)):
+  elif(dist <= 2):
     return "A flecha atingiu a região B"
-  elif((x <= 3 and x >= -3) and (y <= 3 and y >= -3)):
+  elif(dist <= 3):
     return "A flecha atingiu a região C"
-  elif((x <= 4 and x >= -4) and (y <= 4 and y >= -4)):
+  elif(dist <= 4):
     return "A flecha atingiu a região D"
-  elif((x <= 5 and x >= -5) and (y <= 5 and y >= -5)):
+  elif(dist <= 5):
     return "A flecha atingiu a região E"
   else:
     return "A flecha foi para fora do alvo"
 
 def main():
-  x = int(input("Qual a posição x da flecha? "))
-  y = int(input("Qual a posição y da flecha? "))
-  print(posicao(x,y))
+  x = float(input("Qual a posição x da flecha? "))
+  y = float(input("Qual a posição y da flecha? "))
+  dist = ((x - 0)) + ((y ** 2)) ** 0.5
+  print(posicao(dist))
 main()
 """
 
@@ -69,3 +89,5 @@ valor_dolar = 5.07
 x_dolares = int(input("Quantas dólares você quer comprar? "))
 print("Você precisa de " + str(valor_dolar * x_dolares) + " reais")
 """
+
+
