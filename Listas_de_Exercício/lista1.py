3ade4bad790099c3fac7e22903f3382f8c099bc1
Viniciusmgm <vinicius.mirandagm@gmail.com>
2021-06-13 00:07:42 -0300

    continuação da lista1

--- Listas_de_Exercício/lista1.py
+++ Listas_de_Exercício/lista1.py
@@ -5,10 +5,12 @@
 """
 Entrada: posições de x e y da flecha
 Saída: posição da flecha no alvo 
+
 """
 
 #b)Código em Python:
 """
+Está errado!!!
 def posicao(x,y):
   if((x <= 1 and x >= -1) and (y <= 1 and y >= -1)):
     return "A flecha atingiu a região A"
@@ -35,7 +37,7 @@ main()
 O simbolo de igual(=) é usado para atribuição de um valor a uma variável. Já o simbolo de igualdade(==) é
 utilizado para fazer a comparação entre 2 valores, ou variáveis
 
-Exemplo:
+Exemplos:
 a) Simbolo de = :
 valor = 8
 print(valor) #imprime o número 8, que foi atribuido a variável valor
@@ -46,3 +48,20 @@ valor2 = 6
 print(valor1 == valor2) #Imprime o valor booleano False, pois o valor1 que é 5, não é igual ao valor2 que é 6
 """
 
+#Exercício 3
+
+#Exercício 4
+"""
+O return é usado para retornar um valor da função, assim como pode ser usado para finalizar a execução de uma função
+"""
+
+#Exercício 5
+"""
+a) Não há conversão do tipo da entrada, ou seja, o input irá armazenar uma string, logo, é necessário fazer a conversão de string para int. 
+Após realizar a conversão, é necessário converter novamente em string para poder concatenar a variável na hora de printar 
+
+b) Código resolvido:
+valor_dolar = 5.07
+x_dolares = int(input("Quantas dólares você quer comprar? "))
+print("Você precisa de " + str(valor_dolar * x_dolares) + " reais")
+"""
