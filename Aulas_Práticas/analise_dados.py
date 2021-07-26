0f5bec72e2ce69155086c4dfe7797ab4b9246c85
Viniciusmgm <vinicius.mirandagm@gmail.com>
2021-07-25 21:12:48 -0300

    continuação analise de dados

--- Aulas_Práticas/analise_dados.py
+++ Aulas_Práticas/analise_dados.py
@@ -12,6 +12,17 @@ Atividade
 
 import matplotlib.pyplot as plt
 
+def dissimilaridade(serie_temp1, serie_temp2, janela):
+  serie_temp1 = norm_amplitude(serie_temp1)
+  serie_temp1 = norm_translacao(serie_temp1)
+  serie_temp1 = discretiza(serie_temp1, janela)
+  serie_temp2 = norm_amplitude(serie_temp2)
+  serie_temp2 = norm_translacao(serie_temp2)
+  serie_temp2 = discretiza(serie_temp2, janela)
+  dissim = "formula aqui"
+  print("A dissimilaridade entre as séries é {}".format(dissim))
+
+
 def suavizar(serie, janela):
   nova = serie.copy()
   for i in range(janela, len(serie)):
@@ -48,7 +59,7 @@ def discretiza(serie, janela):
   return nova
 
 def main():
-  #numeros de novos casos de covid-19 no primeiros 61 dias a partir do primeiro caso nas cidades Santo Andr�, S�o Paulo e S�o Bernardo do Campo   
+  #numeros de novos casos de covid-19 no primeiros 61 dias a partir do primeiro caso nas cidades Santo André, São Paulo e São Bernardo do Campo   
   SA = [1,0,1,0,1,0,0,0,0,7,6,1,0,0,23,8,3,8,11,1,1,2,24,15,17,9,3,5,3,10,26,19,17,14,11,10,13,5,11,10,43,5,34,42,48,51,46,13,2,6,38,135,44,33,48,41,4,35,84,104,72]
   SP = [1,0,0,0,0,1,3,0,6,3,0,3,11,15,0,18,0,83,11,58,45,47,0,0,0,0,416,177,145,0,0,189,652,533,397,387,294,116,142,504,689,530,505,149,221,66,287,1059,144,836,684,240,147,527,349,534,575,1298,415,476,1408]
   SBC = [1,0,2,0,1,0,0,0,0,9,0,2,0,0,18,11,8,7,11,7,4,8,40,18,17,4,4,7,6,16,30,24,26,13,3,1,12,29,19,63,42,12,36,55,50,66,23,15,11,12,82,80,39,73,63,15,4,23,88,111,63]
@@ -69,5 +80,9 @@ def main():
 
   #plota_salva(norm_translacao(SA), 'norm_translacao')
 
-  plota_salva(discretiza(SA, 5), 'discretiza')
+  #plota_salva(discretiza(SA, 5), 'discretiza')
+
+  janela = int(input("Qual a janela você deseja? "))
+  dissimilaridade(SA, SP, janela)
+
 main()
---
+++ Listas_de_Exercício/lista3.py
@@ -0,0 +1,20 @@
+#Exercício 1
+"""
+Assim como em um string, é possível se acessar as posições em listas de caracteres, porém a lista possibilita a mudança de um valor ou caractere em uma posição especifíca, já nas strings, isso não é possível.
+"""
+
+#Exercício 2
+
+
+#Exercício 3
+
+
+#Exercício 4
+"""
+def vezes_palavra(texto, palavra):
+  texto = texto.split(" ")
+  num_vezes = texto.count(palavra)
+  print("A palavra {} aparece no texto {} vezes".format(palavra, num_vezes))
+
+vezes_palavra("só sei que nada sei", "sei")
+"""
